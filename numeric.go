9c7f94667af8d436e11a09d3791e4f8e47715d88
Kaihua <qinkaihua@outlook.com>
2021-11-15 22:17:31 +0800

    NumericList

--- numeric.go
+++ numeric.go
@@ -28,3 +28,31 @@ func (n *Numeric) Scan(value interface{}) error {
 func (n Numeric) Value() (driver.Value, error) {
 	return n.Int.String(), nil
 }
+
+type NumericList struct {
+	List []*big.Int
+}
+
+func (NumericList) GormDataType() string {
+	return "numeric[]"
+}
+
+func (nl *NumericList) Scan(value interface{}) error {
+	for _, s := range value.([]string) {
+		i, ok := new(big.Int).SetString(s, 10)
+		if !ok {
+			return errors.New(fmt.Sprint("Failed to unmarshal value:", s))
+		}
+		nl.List = append(nl.List, i)
+	}
+
+	return nil
+}
+
+func (nl NumericList) Value() (driver.Value, error) {
+	var v []string
+	for _, i := range nl.List {
+		v = append(v, i.String())
+	}
+	return v, nil
+}
